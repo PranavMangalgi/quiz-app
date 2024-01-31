9de8ed0a630085ac378f4832b51f43cd7e01793c
pranavmangalgi <pranavmangalgi@gmail.com>
2024-01-31 17:41:14 +0530

    resinstalled dependenices pt 2

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,6 @@
-const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
 const handleSignUp = async (req, res, next) => {
   try {
